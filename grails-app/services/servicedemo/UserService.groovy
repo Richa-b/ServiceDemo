12c4bf2eec3d17ea6610afd7396147f5693e29c8
Richa <richa@tothenew.com>
2016-03-06 01:02:58 +0530

    Showing Demo For Transactional Behaviour in Service

--- grails-app/services/servicedemo/UserService.groovy
+++ grails-app/services/servicedemo/UserService.groovy
@@ -3,6 +3,11 @@ package servicedemo
 import grails.transaction.Transactional
 import groovy.sql.Sql
 
+import java.sql.SQLException
+
+//@Transactional(rollbackFor = Exception.class)
+
+
 @Transactional
 class UserService {
 
@@ -10,6 +15,16 @@ class UserService {
 
         User user = new User(firstName:"Ram" , lastName : "Malhotra" , userId: "1")
         user.save()
+       // throw new RuntimeException()
+        //throw new IOException()
+        user
+
+    }
+
+    //if we use @Transactional on any service method then only that method is transactional. i.e
+    //@Transactional
+    def dummyMethod() {
+
     }
 
 }
